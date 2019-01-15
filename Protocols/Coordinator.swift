02278cc9f0abfe6aa8753559c8cdf97250cfdf5e
Giulio Lombardo <giulio.lombardo@gmail.com>
2019-01-15 14:30:28 +0100

    Add Coordinator protocol

---
+++ Protocols/Coordinator.swift
@@ -0,0 +1,58 @@
+//
+//  Coordinator.swift
+//  Coordinator
+//
+//  Created by Giulio Lombardo on 15/01/2019.
+//
+//  MIT License
+//
+//  Copyright (c) 2019 Giulio Lombardo
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to
+//  deal in the Software without restriction, including without limitation the
+//  rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+//  sell copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+//  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+//  IN THE SOFTWARE.
+//
+
+protocol Coordinator: AnyObject {
+    var parentCoordinator: Coordinator? { get set }
+    var childCoordinators: [Coordinator] { get set }
+
+    func start()
+    func stop()
+}
+
+extension Coordinator {
+    func addChildCoordinator(coordinator: Coordinator) {
+        childCoordinators.append(coordinator)
+    }
+
+    func removeChildCoordinator(coordinator: Coordinator) {
+        coordinator.stop()
+
+        childCoordinators = childCoordinators.filter({ childCoordinator -> Bool in
+            childCoordinator !== coordinator
+        })
+    }
+
+    func removeAllChildCoordinators() {
+        childCoordinators.forEach({ coordinator in
+            coordinator.stop()
+        })
+
+        childCoordinators.removeAll()
+    }
+}
